c58fc03fe494aa8c0deb453e9b5f51db3ee448ba
Iago Rodrigues <yagomello16@gmail.com>
2024-11-02 13:11:43 -0300

    feat(ProjectsPage): add fetch projects

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,20 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
+
+const projects = ref<Tables<'projects'>[] | null>()
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select('*')
+
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  projects.value = data
+})()
+</script>
 
 <template>
-  <div>
-    <h1>Projects page</h1>
-    <ul>
-      <li>
-        <RouterLink to="/">Home</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/projects/1">Project 1</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/projects/2">Project 2</RouterLink>
-      </li>
-    </ul>
-  </div>
+  <RouterLink to="/">Home</RouterLink>
+
+  <section>
+    <h2>Projects</h2>
+
+    <table style="width: 100%">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>{{ project.name }}</td>
+          <td>
+            {{ project.status === 'in-progress' ? 'In Progress' : 'Completed' }}
+          </td>
+          <td style="text-align: center">
+            <RouterLink :to="`/projects/${project.id}`">View</RouterLink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <style scoped></style>
