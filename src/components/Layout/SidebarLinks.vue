6c68c475edcc01c3dce746d90d4af68824fcf6e0
Iago Rodrigues <yagomello16@gmail.com>
2024-11-03 10:30:19 -0300

    fix: sidebar links not active on inner routes

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -16,7 +16,7 @@ defineProps<{
     :key="link.title"
     :to="link.to"
     class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
-    exactActiveClass="text-primary bg-muted"
+    activeClass="text-primary bg-muted"
   >
     <iconify-icon :icon="link.icon" />
     <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
