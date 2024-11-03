9f34c42cc1eac0649daec9b3b88a17b2677c47a7
Iago Rodrigues <yagomello16@gmail.com>
2024-11-03 09:36:04 -0300

    feat: add layout comps and clean up app.vue

---
+++ src/components/Layout/SidebarLinks.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+export interface LinkProp {
+  title: string
+  to: string
+  icon: string
+}
+
+defineProps<{
+  links: LinkProp[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="link in links"
+    :key="link.title"
+    :to="link.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+    exactActiveClass="text-primary bg-muted"
+  >
+    <iconify-icon :icon="link.icon" />
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+</template>
